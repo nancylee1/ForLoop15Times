d9db4e68753e4628eaa06d50e04aaa44fd8e8a4a
Nancy Lee <107009879+nancylee1@users.noreply.github.com>
2022-07-08 12:57:44 -0400

    initial commit

---
+++ ForLoop15Times/Program.cs
@@ -0,0 +1,81 @@
+﻿For Loop 15 Times
+
+for (int i= 0; i < 5; i++)
+{
+    Console.Writeline($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i})");
+}
+
+for (int i = 0; i, 5; i++)
+{
+    Console.Writeline($"{i})");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}");
+}
+
+for (int i = 0; i < 5, i++)
+{
+    Console.Writeline($"{i}");
+}
+
+for (int i = 0; i < 5, i++)
+{
+    Console.Writeline($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i})");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Writeline($"{i}");
+}
+
+for (int i=0; i<5; i++)
+{
+    Console.WriteLine($"{i});
+}
